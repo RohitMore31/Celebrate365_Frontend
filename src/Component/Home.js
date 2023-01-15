512fbb07266ed8066a6b3099bcecf08bbe31fdda
Rohit <rohitmore1848@gmail.com>
2023-01-15 20:54:16 +0530

    home page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,11 +3,21 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='homecontainer'>
+          <h2>Upcomming..........</h2>
       <div className='upcoming'>
-
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>LastName</th>
+            <th>Date</th>
+          </tr>
+          <tr>
+            { }
+          </tr>
+        </table>
       </div>
-      <div className='recent'>
-        
+          <h2>......Recent</h2>
+      <div className='upcoming'>
       </div>
     </div>
   )
