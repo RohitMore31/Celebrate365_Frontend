a27dd8bcf1881752058c7623dc6d617e78d1eef9
Rohit <rohitmore1848@gmail.com>
2023-03-13 22:40:21 +0530

    ip

--- src/Component/ipadd.js
+++ src/Component/ipadd.js
@@ -1,6 +1,7 @@
 
 // export let ipadd = {ipa:'celebrate365.onrender.com'};
-export let ipadd = {ipa:'localhost'};
+// export let ipadd = {ipa:'localhost'};
+export let ipadd = {ipa:'15.207.98.56'};
 
 // import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
